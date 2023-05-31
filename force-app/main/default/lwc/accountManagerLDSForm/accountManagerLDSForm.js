b02ad8c3a718c381b071b3b299e2e4574cf23720
Manish kumar sahu <102276444+mimi-0p@users.noreply.github.com>
2023-06-01 01:36:25 +0530

    lwc form view and edit

---
+++ force-app/main/default/lwc/accountManagerLDSForm/accountManagerLDSForm.js
@@ -0,0 +1,8 @@
+import { LightningElement ,track} from 'lwc';
+
+export default class AccountManagerLDSForm extends LightningElement {
+    @track recordId;
+    successHandler(event){
+        this.recordId=event.detail.id;
+    }
+}
