d0eace80d05b82ef2fa8d4894be71a69ad3c50a8
Manish kumar sahu <102276444+mimi-0p@users.noreply.github.com>
2023-06-02 00:52:41 +0530

    new

--- force-app/main/default/lwc/accountManageApex/accountManageApex.js
+++ force-app/main/default/lwc/accountManageApex/accountManageApex.js
@@ -1,10 +1,10 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement,track } from 'lwc';
 import getAllAccount from '@salesforce/apex/accountManage.getAccount';
 
 export default class AccountManageApex extends LightningElement {
 
-    @wire(getAllAccount)
-    accounts;
+    @track numberOfRecords;
+    @track accounts;
 
     get responseReceived(){
         if(this.accounts){
@@ -12,4 +12,19 @@ export default class AccountManageApex extends LightningElement {
         }
         return false;
     }
+
+    numberOfAccountChangeHandler(event){
+        this.numberOfRecords = event.target.value;
+        console.log('number',this.numberOfRecords);
+    }
+
+    getAccounts(){
+        console.log('comes');
+        getAllAccounts({numberOfRecords:this.numberOfRecords}).then(Response =>{
+            this.accounts = Response;
+        }).catch(error =>{
+            console.error('Error in getting the accounts', error.body.message);
+        })
+    
+        }
 }
